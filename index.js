86b5b7bfc84a710b45c17df804632b1958f1ebd7
Rubén <rubenofen@gmail.com>
2020-05-03 19:05:15 +0200

    listen in heroku port

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on *: ${process.env.PORT}`);
 });
