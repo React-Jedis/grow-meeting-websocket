5c445b31a5a458c121d4e33a186f450eca0d4b1b
Rubén <rubenofen@gmail.com>
2020-05-03 19:07:27 +0200

    new port message

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ io.on("connection", (socket) => {
 });
 
 http.listen(process.env.PORT || 3000, () => {
-  console.log(`listening on *: ${process.env.PORT}`);
+  console.log(`listening on port: ${process.env.PORT}`);
 });
