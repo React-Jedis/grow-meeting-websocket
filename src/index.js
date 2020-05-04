50907b24790f1c629a1f3e916d3d78d7e09fddae
Rubén <rubenofen@gmail.com>
2020-05-04 14:52:13 +0200

    logging

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
   socket.on("action", (msg) => {
-    console.log(`Emiting ${msg} to ${msg.room}`);
+    console.log(`Emiting ${Json.stringify(msg)} to ${msg.room}`);
     io.to(msg.room).emit("action", msg);
   });
   socket.on("join", (msg) => {
